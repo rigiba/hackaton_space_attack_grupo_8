acdc800209c2369838847ab9741940c610896ce1
Richard Dev <rigiba@gmail.com>
2021-06-27 11:14:40 -0500

    [RGB] - Include websocket

---
+++ Backend/bd.js
@@ -0,0 +1,10 @@
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://adminmongo:5Vv95hmdl0tjlSpK@clusterdatabases.nj2ti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+//5Vv95hmdl0tjlSpK
